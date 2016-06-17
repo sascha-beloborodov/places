b8bab8eae7fac5ada44abfd8e2702913ea209c9a
sascha-beloborodov <a.beloborodov@hotmail.com>
2016-06-17 14:50:20 +0300

    first commit

---
+++ database/migrations/2016_06_16_214336_create_places_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlacesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->string('address');
+            $table->float('longitude', 8, 6);
+            $table->float('latitude', 8, 6);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('places');
+    }
+}
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Faker\Factory as Faker;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach (range(1, 200000) as $index) {
+            DB::table('places')->insert([
+                'name' => $faker->text(mt_rand(5, 80)),
+                'description' => $faker->text(mt_rand(50, 300)),
+                'address' => $faker->text(mt_rand(20, 150)),
+                'longitude' => $faker->randomFloat(6, 0, 180),
+                'latitude' => $faker->randomFloat(6, 0, 90)
+            ]);
+        }
+        
+//        Model::unguard();
+
+        // $this->call(UserTableSeeder::class);
+
+//        Model::reguard();
+    }
+}
